8231db0d39074833b02f65de203e94997b76f891
Kaio Macedo <kaio@sefaz.to.gov.br>
2026-02-09 17:38:22 -0300

    - adicionando campo insights em usuario para observacoes de melhorias (com possibilidade de usar IA para enviar insights para o usuario referente a sua performance em resolucao de chamados)

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,11 @@
 <?php
 
 use App\Http\Controllers\TicketController;
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth.token')->group(function (): void {
+    Route::get('/dashboard', DashboardController::class);
     Route::get('/tickets', [TicketController::class, 'index']);
     Route::get('/tickets/{ticket}', [TicketController::class, 'show']);
     Route::post('/tickets', [TicketController::class, 'store']);
